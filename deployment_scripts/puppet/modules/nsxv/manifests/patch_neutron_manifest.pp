ac7f96d72cbeb9c8793a647ec9c5c56ecb5b08c3
Artem Savinov <asavinov@mirantis.com>
2015-10-28 11:23:12 +0300

    Remove customized .deb package python-nova out of plugin and remove manifest that installs it
    * Remove python-nova.deb package and remove manifest that installs it
    * Remove manifest that change plugin repository settings
    * Change neutron policy for liberty release from vmware plugin
    * Change neutron manifests for work on liberty release
    
    Change-Id: If0c64ae24fd63eb733550669e74cfe0cc83756db

--- deployment_scripts/puppet/modules/nsxv/manifests/patch_neutron_manifest.pp
+++ deployment_scripts/puppet/modules/nsxv/manifests/patch_neutron_manifest.pp
@@ -1,17 +1,24 @@
 class nsxv::patch_neutron_manifest (
   $manifest_file = '/etc/puppet/modules/osnailyfacter/modular/openstack-network/openstack-network-controller.pp',
-  $policy_file = '/etc/puppet/files/policy.json',
+  $policy_dir = '/etc/neutron/policy.d'
 ) {
+  $neutron_dir = '/etc/neutron'
+
   file { $manifest_file:
     ensure  => file,
     source  => "puppet:///modules/${module_name}/openstack-network-controller.pp",
     mode    => '0644',
     replace => true,
   }
-  file { $policy_file:
-    ensure  => file,
-    source  => "puppet:///modules/${module_name}/policy.json",
-    mode    => '0644',
-    replace => true,
+  file { $neutron_dir:
+    ensure  => directory,
+    mode    => '0755',
+  }
+  file { $policy_dir:
+    ensure  => directory,
+    mode    => '0755',
+    source  => "puppet:///modules/${module_name}/policy.d",
+    recurse => true,
+    require => File[$neutron_dir],
   }
 }
