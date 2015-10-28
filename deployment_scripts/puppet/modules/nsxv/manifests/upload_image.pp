3d7cbebcd8e66ef9259ab273e6f6b02e8e8d9c69
Artem Savinov <asavinov@mirantis.com>
2015-10-28 14:45:32 +0300

    Override TestVM-VMDK to TestVM-TCL (using the name 'TestVM-VMDK')
    * Add simple cloud-init to TinyCoreLinux image (for SSH key integration)
    * Rebuild deb with TinyCoreLinux image
    * Delete image 'TestVM-VMDK' and upload TinyCoreLinux image with name
      'TestVM-VMDK'
    
    Change-Id: I464840b81a6d16f27de9bb2965762ab3017b2feb

--- deployment_scripts/puppet/modules/nsxv/manifests/upload_image.pp
+++ deployment_scripts/puppet/modules/nsxv/manifests/upload_image.pp
@@ -6,12 +6,24 @@ class nsxv::upload_image (
   $disk_format = 'vmdk',
   $container_format = 'bare',
   $public = 'true',
-  $img_name = 'TestVM-TCL',
+  $img_name = 'TestVM-VMDK',
   $img_path = '/usr/share/tcl-testvm/tcl.vmdk',
   $min_ram = 128,
 ) {
   package { 'tcl-testvm':
     ensure => latest,
+  }
+  exec { 'remove-img':
+    path        => '/sbin:/usr/sbin:/bin:/usr/bin',
+    environment => [
+      "OS_TENANT_NAME=${os_tenant_name}",
+      "OS_USERNAME=${os_username}",
+      "OS_PASSWORD=${os_password}",
+      "OS_AUTH_URL=${os_auth_url}",
+      'OS_ENDPOINT_TYPE=internalURL',
+    ],
+    command     => "glance -k image-delete ${img_name}",
+    unless      => "glance -k image-list && (test -z \"$(glance -k image-list | grep ${img_name})\")",
   }
   exec { 'upload-img':
     path        => '/sbin:/usr/sbin:/bin:/usr/bin',
@@ -24,6 +36,6 @@ class nsxv::upload_image (
     ],
     command     => "glance -k image-create --name=${img_name} --is-public=${public} --container-format=${container_format} --disk-format=${disk_format} --property hypervisor_type='vmware' --property vmware_disktype='streamOptimized' --property vmware_adaptertype='lsilogic' --min-ram ${min_ram} < ${img_path}",
     unless      => "glance -k image-list && (glance -k image-list | grep ${img_name})",
-    require     => Package['tcl-testvm'],
+    require     => [Package['tcl-testvm'],Exec['remove-img']],
   }
 }
