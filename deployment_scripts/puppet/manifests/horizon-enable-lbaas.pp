a7e77b3d7728bc48c68a21286834a86ddbd6576c
Artem Savinov <asavinov@mirantis.com>
2015-11-09 17:24:13 +0300

    Adapt plugin deployment tasks to changed Neutron deployment
    * Adapt deploy nsxv neutron plugin for granular neutron deployment
    * Create predifined networks/router
    * Enable loadbalancer v1 support
    
    Change-Id: I5fdf351957fddba3fa61190b251a71fb5c907d3b

---
+++ deployment_scripts/puppet/manifests/horizon-enable-lbaas.pp
@@ -0,0 +1,19 @@
+notice('fuel-plugin-nsxv: horizon-enable-lbaas.pp')
+
+$use_neutron = hiera('use_neutron', false)
+
+if $use_neutron {
+  $horizon_settings_file ='/etc/openstack-dashboard/local_settings.py'
+  $apache_service        ='apache2'
+
+  exec { 'enable_lbaas':
+    command  => "sed -ri \"s/^(\\s*)'enable_lb':.*/\\1'enable_lb': True,/g\" $horizon_settings_file",
+    unless   => "egrep \"^\\s*'enable_lb':\\s*True\" $horizon_settings_file",
+    path     => '/bin:/usr/bin',
+    provider => 'shell',
+  }
+  service{ $apache_service:
+    ensure => 'running',
+  }
+  Exec['enable_lbaas'] ~> Service[$apache_service]
+}
