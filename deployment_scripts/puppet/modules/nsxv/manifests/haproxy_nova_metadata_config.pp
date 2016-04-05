34c48042ed5805bc90158fd6de99c5720bd35ea5
Artem Savinov <asavinov@mirantis.com>
2016-04-05 14:03:24 +0300

    Explicit restart haproxy after changes timeout and listen ip
    * Explicit restart haproxy after changes timeout for haproxy config to
      neutron and listen ip for haproxy config to nova metadata
    
    Change-Id: I465d08ff7c08ef8ccb24ff4594e24016de198a97
    Closes-Bug: 1565707
    Related-Bug: 1560377

--- deployment_scripts/puppet/modules/nsxv/manifests/haproxy_nova_metadata_config.pp
+++ deployment_scripts/puppet/modules/nsxv/manifests/haproxy_nova_metadata_config.pp
@@ -1,11 +1,14 @@
 class nsxv::haproxy_nova_metadata_config (
   $metadata_ha_conf = '/etc/haproxy/conf.d/060-nova-metadata-api.cfg',
 ) {
+  include openstack::ha::haproxy_restart
+
   $public_vip = hiera('public_vip')
 
   file_line { 'metadata_public_listen':
-    path  => $metadata_ha_conf,
-    after => 'listen nova-metadata-api',
-    line  => "  bind ${public_vip}:8775",
+    path   => $metadata_ha_conf,
+    after  => 'listen nova-metadata-api',
+    line   => "  bind ${public_vip}:8775",
+    notify => Exec['haproxy-restart'],
   }
 }
--- deployment_scripts/puppet/modules/nsxv/manifests/neutron_haproxy_config.pp
+++ deployment_scripts/puppet/modules/nsxv/manifests/neutron_haproxy_config.pp
@@ -1,4 +1,6 @@
 class nsxv::neutron_haproxy_config {
+  include openstack::ha::haproxy_restart
+
   file { '/tmp/neutron-haproxy-config.sh':
     ensure  => file,
     mode    => '0755',
@@ -6,9 +8,10 @@ class nsxv::neutron_haproxy_config {
     replace => true,
   }
   exec { 'neutron active/backup mode':
-    command     => '/tmp/neutron-haproxy-config.sh',
-    logoutput   => on_failure,
-    provider    => 'shell',
-    require     => File['/tmp/neutron-haproxy-config.sh'],
+    command   => '/tmp/neutron-haproxy-config.sh',
+    logoutput => on_failure,
+    provider  => 'shell',
+    require   => File['/tmp/neutron-haproxy-config.sh'],
+    notify    => Exec['haproxy-restart']
   }
 }
