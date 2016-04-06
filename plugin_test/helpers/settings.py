18921ab86b2a1987ee77ab0dde3cd3b264cda92a
Denis Deryabin <dderyabin@mirantis.com>
2016-04-06 13:41:41 +0300

    add system tests and some improvements
    * automated test case nsxv_floating_ip_to_public
    * automated test case nsxv_public_network_availability
    * move test variables to module
    * add OpenStack helper class
    
    Change-Id: I3654f45f50d088013948ce1c12fea21e9e955e17

---
+++ plugin_test/helpers/settings.py
@@ -0,0 +1,54 @@
+"""Copyright 2016 Mirantis, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may
+not use this file except in compliance with the License. You may obtain
+copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+License for the specific language governing permissions and limitations
+under the License.
+"""
+import os
+from distutils.util import strtobool
+
+
+WAIT_FOR_COMMAND = 60 * 3  # 3 minutes
+WAIT_FOR_LONG_DEPLOY = 60 * 180  # 180 minutes
+
+EXT_IP = '8.8.8.8'  # Google DNS ^_^
+PRIVATE_NET = "admin_internal_net"
+ADMIN_NET = 'admin_floating_net'
+DEFAULT_ROUTER_NAME = 'router04'
+METADATA_IP = '169.254.169.254'
+
+NSXV_PLUGIN_PATH = os.environ.get('NSXV_PLUGIN_PATH')
+
+plugin_configuration = {
+    'nsxv_manager_host/value': os.environ.get('NSXV_MANAGER_IP'),
+    'nsxv_insecure/value': strtobool(os.environ.get('NSXV_INSECURE')),
+    'nsxv_user/value': os.environ.get('NSXV_USER'),
+    'nsxv_password/value': os.environ.get('NSXV_PASSWORD'),
+    'nsxv_datacenter_moid/value': os.environ.get('NSXV_DATACENTER_MOID'),
+    'nsxv_resource_pool_id/value': os.environ.get('NSXV_RESOURCE_POOL_ID'),
+    'nsxv_datastore_id/value': os.environ.get('NSXV_DATASTORE_ID'),
+    'nsxv_external_network/value': os.environ.get('NSXV_EXTERNAL_NETWORK'),
+    'nsxv_vdn_scope_id/value': os.environ.get('NSXV_VDN_SCOPE_ID'),
+    'nsxv_dvs_id/value': os.environ.get('NSXV_DVS_ID'),
+    'nsxv_backup_edge_pool/value': os.environ.get('NSXV_BACKUP_EDGE_POOL'),
+    'nsxv_mgt_net_moid/value': os.environ.get('NSXV_MGT_NET_MOID'),
+    'nsxv_mgt_net_proxy_ips/value': os.environ.get('NSXV_MGT_NET_PROXY_IPS'),
+    'nsxv_mgt_net_proxy_netmask/value': os.environ.get(
+        'NSXV_MGT_NET_PROXY_NETMASK'),
+    'nsxv_mgt_net_default_gateway/value': os.environ.get(
+        'NSXV_MGT_NET_DEFAULT_GW'),
+    'nsxv_floating_ip_range/value': os.environ.get('NSXV_FLOATING_IP_RANGE'),
+    'nsxv_floating_net_cidr/value': os.environ.get('NSXV_FLOATING_NET_CIDR'),
+    'nsxv_internal_net_cidr/value': os.environ.get('NSXV_INTERNAL_NET_CIDR'),
+    'nsxv_floating_net_gw/value': os.environ.get('NSXV_FLOATING_NET_GW'),
+    'nsxv_internal_net_dns/value': os.environ.get('NSXV_INTERNAL_NET_DNS'),
+    'nsxv_edge_ha/value': strtobool(os.environ.get('NSXV_EDGE_HA')),
+}
