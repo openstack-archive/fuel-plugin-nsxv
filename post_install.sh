4d3ab80a9b71ea97f7694b6cef24e5f1c3b631d1
Artem Savinov <asavinov@mirantis.com>
2016-04-13 13:55:04 +0300

    Disable incompatible roles with NSXv
    * Disable incompatible roles with NSXv
      compute,ironic,cinder,cinder-block-device
    
    Change-Id: I9dfe7ae1abd30f3846740ebc6f102677465282af

--- post_install.sh
+++ post_install.sh
@@ -1,7 +1,2 @@
-keyFile='/var/www/nailgun/plugins/%{name}/deployment_scripts/puppet/modules/nsxv/files/compute_vmware_key'
-if [ -f $keyFile ]; then
-  echo "  Ssh key file exists, skip generation"
-else
-  echo -n "  Ssh key file for nsxv plugin not found, "
-  ssh-keygen -t rsa -b 2048 -N "" -f $keyFile
-fi
+#!/bin/bash
+/var/www/nailgun/plugins/%{name}/deployment_scripts/disable_role.py
