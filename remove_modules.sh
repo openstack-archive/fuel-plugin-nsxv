4fb606c5a03137b7bf4f29379280cb953a14639d
Artem Savinov <asavinov@mirantis.com>
2016-04-15 13:14:25 +0300

    Fix warnings from code check tools
    * Fixed shellcheck warnings
    
    Change-Id: Iae750a34b9aec24d499ca72524c7c915f8b76695

--- remove_modules.sh
+++ remove_modules.sh
@@ -1,15 +1,15 @@
-#!/bin/sh
+#!/bin/sh -e
 # remove all external puppet modules
 
 PLUGIN_MOD_DIR="deployment_scripts/puppet/modules"
 
-dir=`dirname $0`
+dir=$(dirname $0)
 cd "${dir}" || exit 1
 
-cat Puppetfile | grep "^mod '" | awk -F "'" '{ print $2 }' | while read module; do
+grep "^mod '" Puppetfile| awk -F "'" '{ print $2 }' | while read module; do
   if [ -d "${PLUGIN_MOD_DIR}/${module}" ]; then
     echo "Remove: ${PLUGIN_MOD_DIR}/${module}"
-    rm -rf "${PLUGIN_MOD_DIR}/${module}"
+    rm -rf "${PLUGIN_MOD_DIR:?}/${module}"
   fi
 done
 rm -f 'Puppetfile.lock'
