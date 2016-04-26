2940fc9aff6a2078293b53e1c474539eb2cf040e
Artem Savinov <asavinov@mirantis.com>
2016-04-26 11:15:42 +0300

    Fix warnings from code check utilites(shellcheck/flake8)
    Change-Id: I7c3759639b3edca41860c1c47590237afa3e8092

--- post_install.sh
+++ post_install.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/usr/bin/python /var/www/nailgun/plugins/%{name}/deployment_scripts/disable_role.py
+/usr/bin/python "/var/www/nailgun/plugins/%{name}/deployment_scripts/disable_role.py"
