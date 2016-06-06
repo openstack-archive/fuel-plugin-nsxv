aad51fdbaff8e6fff78ee53895da64afaea41e77
Igor Zinovik <izinovik@mirantis.com>
2016-06-06 16:19:47 +0300

    Rework neutron-server configuration and start order
    Quite complex problem appears when we try to deploy several controllers
    simultaneously. Neutron on primary controller start and tries to
    inialize metadata router. Experiments showed that NSX Manager responds
    with <state>true</state> and <edgeStatus>green</edgeStatus> right after
    edge got deployed, but in fact neutron-server fail to start on one or
    several secondary controllers with metadata initialization error.
    
    We tried to use ruby script that polls NSX Manager asking about metadata
    router state, but problem still occurs even if NSX Manager reports that
    metadata router is in "ready" state.
    
    Another option was to use sleep for some amount. Even with sleep time up
    to 120 seconds issue still occurs. It is not possible to enlarge timeout
    even more.
    
    This commit tries to solve the problem this way: we split neutron-server
    package installation, configuration and service start steps into
    separate deployment tasks.  Then we start neutron-server right after
    "deploy_end" task.
    
    Following tasks we splitted/renamed:
    * openstack-network-neutron-start -> nsxv-neutron-configure-common
    				 `-> nsxv-configure-common
    				 `-> nsxv-configure-primary-controller
    				 `-> nsxv-configure-controller
    				 `-> neutron-start-primary-controller
    				 `-> neutron-start-controller
    * nsxv-install -> nsxv-install-packages
    
    Move hardcoded parameters to ::nsxv::params class.
    
    While here put in some whitespaces to conform Puppet style guide.
    
    Closes-bug: #1587074
    Change-Id: I46064bd9d49a1572cc7b2d015f073d3e3b5e83b4

---
+++ deployment_scripts/puppet/manifests/nsxv-configure-controller.pp
@@ -0,0 +1,5 @@
+notice('fuel-plugin-nsxv: nsxv-configure-controller.pp')
+
+include nsxv::params
+
+nsx_config { "$::nsxv::params::plugin_name/metadata_initializer": value => 'false' }
---
+++ deployment_scripts/puppet/manifests/nsxv-configure-primary-controller.pp
@@ -0,0 +1,9 @@
+notice('fuel-plugin-nsxv: nsxv-configure-primary-controller.pp')
+
+include ::nsxv::params
+
+$settings = hiera($::nsxv::params::plugin_name)
+
+$metadata_initializer = $settings['nsxv_metadata_initializer']
+
+nsx_config { "$::nsxv::params::plugin_name/metadata_initializer": value => $metadata_initializer }
