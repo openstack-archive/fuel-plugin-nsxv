52a16771a6e10a2168733355fbfa17597529b364
Artem Savinov <asavinov@mirantis.com>
2016-06-27 15:32:54 +0300

    Return checkbox 'nsxv_metadata_insecure'
    Due to #1590840 parameters
    nsxv_metadata_nova_client_priv_key/nsxv_metadata_nova_client_cert
    thidden
    
    Change-Id: I8ee1107d24aa79fb02b3b91d79dbd3ddd10a9a83
    Closes-Bug: #1590407

--- deployment_scripts/puppet/modules/nsxv/files/haproxy-nova-metadata-config.sh
+++ deployment_scripts/puppet/modules/nsxv/files/haproxy-nova-metadata-config.sh
@@ -2,21 +2,17 @@
 novaHaproxyConf="$(find /etc/haproxy/conf.d -name '*nova-metadata-api*')"
 tempFile="$(mktemp)"
 
-awk -v metadata_listen="$1" '
-BEGIN {
-  ipListen=0
-}
+awk -v metadata_listen="$1" -v metadata_insecure="$2" -v metadata_crt_key_file="$3" '
 {
-  if ($1 == "bind") {
-      if ($2 == metadata_listen) {
-        ipListen=1
-      }
-  }
+  if ($1 == "bind") { next }
+  if ($1 == "http-request") { next }
   print $0
 }
 END {
-  if (ipListen == 0) {
+  if (metadata_insecure == "false") {
+    print "  bind",metadata_listen,"ssl crt",metadata_crt_key_file,"no-sslv3 no-tls-tickets ciphers AES128+EECDH:AES128+EDH:AES256+EECDH:AES256+EDH"
+    print "  http-request  set-header X-Forwarded-Proto https if { ssl_fc }"
+  } else {
     print "  bind",metadata_listen
   }
-
 } ' $novaHaproxyConf > $tempFile && mv -f $tempFile $novaHaproxyConf
--- deployment_scripts/puppet/modules/nsxv/manifests/haproxy_nova_metadata_config.pp
+++ deployment_scripts/puppet/modules/nsxv/manifests/haproxy_nova_metadata_config.pp
@@ -1,5 +1,7 @@
 class nsxv::haproxy_nova_metadata_config (
   $metadata_listen,
+  $metadata_insecure,
+  $metadata_crt_key_file,
 ) {
   file { '/tmp/haproxy-nova-metadata-config.sh':
     ensure  => file,
@@ -8,9 +10,24 @@ class nsxv::haproxy_nova_metadata_config (
     replace => true,
   }
   exec { 'set nova metadata listen ip':
-    command   => "/tmp/haproxy-nova-metadata-config.sh ${metadata_listen}",
+    command   => "/tmp/haproxy-nova-metadata-config.sh ${metadata_listen} ${metadata_insecure} ${metadata_crt_key_file}",
     logoutput => on_failure,
     provider  => 'shell',
     require   => File['/tmp/haproxy-nova-metadata-config.sh'],
+  }
+
+  if ! $metadata_insecure {
+    file { '/tmp/generate_haproxy_key.sh':
+      ensure  => file,
+      mode    => '0755',
+      source  => "puppet:///modules/${module_name}/generate_haproxy_key.sh",
+      replace => true,
+    }
+    exec { 'generate key/cert for nova metadata':
+      command   => "/tmp/generate_haproxy_key.sh ${metadata_crt_key_file}",
+      logoutput => on_failure,
+      provider  => 'shell',
+      require   => File['/tmp/generate_haproxy_key.sh'],
+    }
   }
 }
